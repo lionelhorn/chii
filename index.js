1045dd897253db6859aeeef2ad706e7dddcfe372
Lionel <dev@lionelhorn.com>
2023-01-15 11:30:11 +0100

    build: updated port and added html

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const server = require('./server');
-server.start({port:8080, host: '0.0.0.0', domain: 'chii-service.onrender.com'});
+server.start({port:8080, host: '0.0.0.0', domain: 'chii-service.onrender.com', cdn:'https://cdn.jsdelivr.net/npm/chii/public/'});
 
