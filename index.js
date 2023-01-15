007431474f859a44d6266f1e9998d66358f90410
Lionel <dev@lionelhorn.com>
2023-01-15 11:04:36 +0100

    build: update for render

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const server = require('./server');
-server.start({port: 443, host:'chii.vercel.app', domain: 'chii.vercel.app'});
+server.start({port: 443, host:'chii-service.onrender.com', domain: 'chii-service.onrender.com'});
 
