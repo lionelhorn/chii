9dbc05d04f6f3d1fefc3daaf9fad720629346b0a
Lionel <dev@lionelhorn.com>
2023-01-15 11:09:07 +0100

    build: update for render

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const server = require('./server');
-server.start({port: 443, host:'chii-service.onrender.com', domain: 'chii-service.onrender.com'});
+server.start({domain: 'chii-service.onrender.com'});
 
